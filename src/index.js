335248a905ce011fd8cd4082095274fb4b28f4d7
Sergey Shishkin <shishkinsa1997@gmail.com>
2025-07-29 18:14:27 +0500

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const num = Math.abs(n);
+  const hundreds = Math.floor(num / 100);
+  const tens = Math.floor((num % 100) / 10);
+  const units = num % 10;
+  let res = '';
+  if (hundreds) {
+    res += `${units}${tens}${hundreds}`;
+  } else if (!hundreds) {
+    res += `${units}${tens}`;
+  }
+  return Number(res);
 };
